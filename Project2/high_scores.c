7e527954b4ab070030d67487718ca887d8c6ea9e
Amethyst Skye <amethystskye.pi@gmail.com>
2022-09-26 12:56:10 -0700

    Refactored a bit

--- Project2/high_scores.c
+++ Project2/high_scores.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include <time.h>
-#include <string.h>
+#include<time.h>
+#include<string.h>
 
 #define num_players 100
 
@@ -9,11 +9,13 @@ int partition(int arr[], int low, int high){
     int temp;
     int pivot = arr[high];    // pivot
     int i = (low - 1);  // Index of smaller element
+    int *large_arr;
+    int large_count = 0;
  
     for (int j = low; j <= high- 1; j++){
         // If current element is smaller than or
         // equal to pivot
-        if (arr[j] <= pivot){
+        if (arr[j] >= pivot){
             i++;    // increment index of smaller element
             temp = arr[i];
             arr[i] = arr[j];
@@ -47,27 +49,18 @@ int cmpfunc (const void * a, const void * b) {
    return ( *(int*)b - *(int*)a );
 }
 
-double use_standard(int score_array[], char *score_list_name){
+double to_sort(int score_array[], char *score_list_name, int sort_alg_type){
     clock_t start, end;
     double cpu_time;
     printf("%s\n", score_list_name);
-    start = clock();
-    qsort(score_array, num_players, sizeof(int), cmpfunc);
-    end = clock();
-    cpu_time = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000000;
-    for (int i = 0; i < num_players; i++)
-        printf("%d\n", score_array[i]);
-    printf("\ntime taken: %lf\n\n", cpu_time);
-    return (cpu_time);
-}
 
-double use_custom(int score_array[], char *score_list_name){
-    clock_t start, end;
-    double cpu_time;
-    printf("%s\n", score_list_name);
     start = clock();
-    quickSort(score_array, 0, num_players -1);
+    if (sort_alg_type == 1)
+        qsort(score_array, num_players, sizeof(int), cmpfunc);
+    else
+        quickSort(score_array, 0, num_players - 1);
     end = clock();
+
     cpu_time = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000000;
     for (int i = 0; i < num_players; i++)
         printf("%d\n", score_array[i]);
@@ -76,7 +69,6 @@ double use_custom(int score_array[], char *score_list_name){
 }
 int main(int argc, char **argv){
     if (argc == 2){
-
         char* list_names[6] = 
         {"SKILL_BREAKDANCING",
         "SKILL_APICULTURE",
@@ -85,71 +77,42 @@ int main(int argc, char **argv){
         "SKILL_SWORD",
         "TOTAL_XP"};
 
-        int breakdancing_scores[num_players],
-        underwater_apiculture_scores[num_players],
-        basket_weaving_scores[num_players],
-        extreme_basket_weaving_scores[num_players],
-        longsword_scores[num_players],
-        total_xp[num_players];
+        /* score_array[i] corresponds to list_names[i] */
+        int score_arrays[6][num_players];
 
         /* Ingest player scores into corrosponding arrays */
         for (int i = 0; i < num_players; i++)
-            scanf("%d %d %d %d %d", &breakdancing_scores[i],
-            &underwater_apiculture_scores[i],
-            &basket_weaving_scores[i],
-            &extreme_basket_weaving_scores[i],
-            &longsword_scores[i]);
+            scanf("%d %d %d %d %d", &score_arrays[0][i],
+            &score_arrays[1][i],
+            &score_arrays[2][i],
+            &score_arrays[3][i],
+            &score_arrays[4][i]);
 
         /* Calculate Total_XP */
         for (int i = 0; i < num_players; i++){
-            total_xp[i] = breakdancing_scores[i] +
-            underwater_apiculture_scores[i] +
-            basket_weaving_scores[i] +
-            extreme_basket_weaving_scores[i] +
-            longsword_scores[i];
+            score_arrays[5][i] = 
+            score_arrays[0][i] +
+            score_arrays[1][i] +
+            score_arrays[2][i] +
+            score_arrays[3][i] +
+            score_arrays[4][i];
         }
 
         clock_t start, end;
-        double cpu_time1, 
-        cpu_time2, 
-        cpu_time3, 
-        cpu_time4, 
-        cpu_time5, 
-        cpu_time6;
+        int sort_alg_type; /* 0 = custom, 1 = standard */
+        double cpu_time, sum_cpu_time = 0;
 
-        if (!strcmp(argv[1], "standard")){ 
-            cpu_time1 = use_standard(breakdancing_scores, list_names[0]),
-            cpu_time2 = use_standard(underwater_apiculture_scores, list_names[1]),
-            cpu_time3 = use_standard(basket_weaving_scores, list_names[2]),
-            cpu_time4 = use_standard(extreme_basket_weaving_scores, list_names[3]),
-            cpu_time5 = use_standard(longsword_scores, list_names[4]),
-            cpu_time6 = use_standard(total_xp, list_names[5]);
+        /* Time to sort! */
+        if (!strcmp(argv[1], "standard")) 
+            sort_alg_type = 1;
+        if (!strcmp(argv[1], "custom"))
+            sort_alg_type = 0;
 
-            printf("total time taken: %lf\n", 
-            cpu_time1 +
-            cpu_time2 +
-            cpu_time3 +
-            cpu_time4 +
-            cpu_time5 +
-            cpu_time6);
+        for (int i = 0; i < 5; i++){
+            cpu_time = to_sort(score_arrays[i], list_names[i], sort_alg_type);
+            sum_cpu_time += cpu_time;
         }
-
-        if (!strcmp(argv[1], "custom")){
-            cpu_time1 = use_custom(breakdancing_scores, list_names[0]),
-            cpu_time2 = use_custom(underwater_apiculture_scores, list_names[1]),
-            cpu_time3 = use_custom(basket_weaving_scores, list_names[2]),
-            cpu_time4 = use_custom(extreme_basket_weaving_scores, list_names[3]),
-            cpu_time5 = use_custom(longsword_scores, list_names[4]),
-            cpu_time6 = use_custom(total_xp, list_names[5]);
-
-            printf("total time taken: %lf\n", 
-            cpu_time1 +
-            cpu_time2 +
-            cpu_time3 +
-            cpu_time4 +
-            cpu_time5 +
-            cpu_time6);
-        }
+        printf("total time taken: %lf\n", sum_cpu_time);
     }
     else
         printf("Please specify either 'standard' or 'custom' sorting upon execution.\n");
